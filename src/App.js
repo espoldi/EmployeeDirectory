9d299092d844770e5aba8fa9cfd62de88d371f57
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-03-20 15:01:15 -0400

    set up class app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 // Components
-import navbar from '.components/navbar';
-import searchbar from '.components/searchbar';
-import sortmenu from '.components/sortmenu';
-import results from '.components/results';
-import infocards from '.components/infocards';
+import NavBar from './components/NavBar';
+import SearchBar from './components/SearchBar';
+import SortMenu from './components/SortMenu';
+import Results from './components/Results';
+import infocard from './components/InfoCard';
 // Materialize
 import M from 'materialize-css';
 // API
 import API from "./utils/API";
 
-function App() {
-  return (
-    <>
-    </>
-  );
+class App extends Component {
+  state = {
+
+  }
+
+  componentDidMount() {
+
+  };
+
+  render() {
+    return (
+      <>
+        <NavBar />
+        <SearchBar />
+        <Results />
+      </>
+    );
+  }
+
 }
 
 export default App;
