ffe45cdd68c1b96b9aaa27d172b1920f4f723456
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-03-20 19:02:02 -0400

    added formatting for cards after search

--- src/components/infocard.js
+++ src/components/infocard.js
@@ -2,8 +2,19 @@ import React from "react";
 
 function InfoCard() {
     return (
-        <>
-        </>
+        <div class="card">
+            <div class="circle card-image">
+                <img src="images/sample-1.jpg" />
+                    <span class="card-title">Card Title</span>
+        </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">This is a link</a>
+                </div>
+            </div>
     );
 }
 
--- src/components/results.js
+++ src/components/results.js
@@ -1,10 +1,14 @@
 import React from "react";
+import InfoCard from "./InfoCard";
 
 function Results() {
     return (
-        <div className="container">
-            
+        <div class="row">
+            <div class="col s12 m5">
+                <InfoCard />
+            </div>
         </div>
+
     );
 }
 
