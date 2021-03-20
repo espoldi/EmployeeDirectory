b4265bb0c01bafd8d440f8d77f66f6b1173a6170
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-03-20 14:48:49 -0400

    imported components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import React from 'react';
-// dont forget to import components
+import React, { Component } from 'react';
+// Components
+import navbar from '.components/navbar';
+import searchbar from '.components/searchbar';
+import sortmenu from '.components/sortmenu';
+import results from '.components/results';
+import infocards from '.components/infocards';
+// Materialize
+import M from 'materialize-css';
+// API
+import API from "./utils/API";
 
 function App() {
   return (
