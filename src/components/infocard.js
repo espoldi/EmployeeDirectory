fdfc91c5427ec71644d5bbfeb4114513c4a242cc
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-03-20 21:59:21 -0400

    fixed class to className

--- src/components/infocard.js
+++ src/components/infocard.js
@@ -2,15 +2,15 @@ import React from "react";
 
 function InfoCard(props) {
     return (
-        <div class="card col s12 m6 l4">
-            <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator" src={props.data.picture.large} />
+        <div className="card col s12 m6 l4">
+            <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator" src={props.data.picture.large} />
             </div>
-            <div class="card-content">
-                <span class="card-title activator grey-text text-darken-4">{props.data.name.title} {props.data.name.last}<i class="material-icons right">more_vert</i></span>
+            <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">{props.data.name.title} {props.data.name.last}<i className="material-icons right">more_vert</i></span>
             </div>
-            <div class="card-reveal">
-                <span class="card-title grey-text text-darken-4">{props.data.name.first} {props.data.name.last}<i class="material-icons right">close</i></span>
+            <div className="card-reveal">
+                <span className="card-title grey-text text-darken-4">{props.data.name.first} {props.data.name.last}<i className="material-icons right">close</i></span>
                 <span>{props.data.gender}, {props.data.dob.age}<br/>
                 Email: {props.data.email}<br/>
                 Phone: {props.data.phone}<br/>
