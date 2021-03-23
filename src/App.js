8d3c55f6b624152d46d10041d2afa1b2b351fff5
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-03-22 21:35:21 -0400

    added refreshing search ability

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import API from "./utils/API";
 class App extends Component {
   state = {
     search: "",
+    dataBackup: [],
     directory: [],
     error: ""
   }
@@ -19,20 +20,38 @@ class App extends Component {
     M.AutoInit();
 
     API.getRandomEmployee()
-      .then(res => this.setState({ directory: res.data.results }))
+      .then(res => this.setState({
+        directory: res.data.results,
+        dataBackup: res.data.results
+      }))
       .catch(err => console.log(err));
   };
 
   handleInputChange = event => {
     const value = event.target.value;
-    this.setState({ search: value });
+    let query = value.toLowerCase().trim();
+    const dataBackup = this.state.dataBackup;
+    let results = [];
+
+    for (let i = 0; i < dataBackup.length; i++) {
+      let person = dataBackup[i];
+      let fullName = (person.name.first + " " + person.name.last).toLowerCase();
+      if (query === fullName.substring(0, query.length)) {
+        results.push(person);
+      }
+    }
+
+    this.setState({
+      search: value,
+      directory: results
+    });
   };
 
   render() {
     return (
       <>
-        <NavBar change={ this.handleInputChange } />
-        <Results employees={ this.state.directory } />
+        <NavBar change={this.handleInputChange} />
+        <Results employees={this.state.directory} />
       </>
     );
   }
